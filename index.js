ff92b47aca7c386fcf50e4efe2edecb63df95351
Emmanuel Ponce <jesus.ponce.supernova@gmail.com>
2020-11-10 18:41:54 -0600

    index config

--- index.js
+++ index.js
@@ -1,5 +1,19 @@
 const express = require('express')
 const app = express()
+const { connection } = require('./database')
+const cors = require('cors')
+const morgan = require('morgan')
+const router = require('./routes')
 require('dotenv').config()
+
+app.use(morgan('dev'))
+app.use(cors())
+
+connection()
+
+app.use(express.json())
+app.use(express.urlencoded({extended:true}))
+
+app.use('/api', router)
 
 app.listen(process.env.PORT, () => console.log(`Server ${process.env.PORT}`))
